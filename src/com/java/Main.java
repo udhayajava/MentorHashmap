50155f3268ac8e1c36a67e0435dee4ff0b1c6f6b
Udhayakumar <udhayaarjvit1060>
2021-10-01 21:32:06 +0530

    final commit

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -1,20 +1,15 @@
 package com.java;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+/*An efficient Java program to return the duplicate characters from given String,
+and also find the maximum occurring of that duplicate character in a given String.
+(use data structure like HashSet or HashMap )*/
 
 public class Main
 {
     public void findDupCharOfGiven(String str) //to find the duplicate char
     {
-/*An efficient Java program to return the duplicate characters from given String,
-and also find the maximum occurring of that duplicate character in a given String.
-(use data structure like HashSet or HashMap )*/
-
-
         // Creating HashMap
-        Map<Character, Integer> hm = new HashMap<Character, Integer>();
+        Map<Character, Integer> hm = new HashMap<>();
         char[] charArray = str.toCharArray();  //converting String to Array of character
 
         for (Character ch : charArray)
@@ -28,7 +23,6 @@ and also find the maximum occurring of that duplicate character in a given Strin
                 hm.put(ch, 1);
             }
         }
-
         Set<Character> keys = hm.keySet(); //Converting to Set to find unique characters
         for (Character ch : keys)
         {
@@ -37,7 +31,33 @@ and also find the maximum occurring of that duplicate character in a given Strin
                 System.out.println(ch + "  is " + hm.get(ch) + " times");
             }
         }
-              }
+    }
+    public void MaxDupCharOfGiven(String str){
+        int maxCount =0;
+            HashMap<Character, Integer> map = new HashMap<>();
+            char[] chars = str.toCharArray();
+            for (int i = 0; i <chars.length ; i++) {
+                char c = chars[i];
+                if(map.containsKey(c)){
+                    int count = map.get(c);
+                    count++;
+                    if(maxCount<count)
+                        maxCount++;
+                    map.put(c, count);
+                }else{
+                    map.put(c, 1);
+                }
+            }
+
+            Set set = map.keySet();
+            Iterator<Character> iterator = set.iterator();
+            while(iterator.hasNext()){
+                char key = iterator.next();
+                if(map.get(key)==maxCount){
+                    System.out.println("Character: " + key + " has occurred max times: " + maxCount);
+                }
+            }
+        }
 
     public static void main(String[] args) {
 
@@ -46,5 +66,6 @@ and also find the maximum occurring of that duplicate character in a given Strin
         System.out.println("Enter a word");
         String str = s.nextLine();
         fd.findDupCharOfGiven(str);
+        fd.MaxDupCharOfGiven(str);
     }
 }
